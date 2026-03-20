dc640f2d5861b7505b540b5bea0eb10f0f1f3713
Jozhua1 <joshuaescalona.on@gmail.com>
2026-03-20 22:36:59 +0800

    Updated specific files

--- Rock,paper,sicssors.py
+++ Rock,paper,sicssors.py
@@ -1,30 +1,46 @@
 import random
 
-emojis = {'r':'✊', 'p':'✋', 's':'✌️'}
-choices = ('r', 'p', 's')
 
-while True:
-    user_choice = input('Choose rock, paper, or scissors (r/p/s): ').lower()
-    if user_choice not in choices:
-        print('Invalid choice. Please choose r, p, or s.')
-        continue
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
 
-    computer_choice = random.choice(choices)
+emojis = {'ROCK':'✊', 'PAPER':'✋', 'SCISSORS':'✌️'}
+choices = tuple(emojis.keys())
 
+def get_user_choice():
+    while True:
+        user_choice = input('rock,paper, or scissors? (r/p/s): ').lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print('Invalid choice')
+
+def display_choices(user_choice, computer_choice):
     print(f'You chose {emojis[user_choice]}')
     print(f'Computer chose {emojis[computer_choice]}')
 
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        print('It\'s a tie!')
+        print('Tie')
     elif (
-         (user_choice == 'r' and computer_choice == 's') or 
-         (user_choice == 'p' and computer_choice == 'r') or 
-         (user_choice == 's' and computer_choice == 'p')):
+        (user_choice == 'ROCK' and computer_choice == 'SCISSORS') or
+        (user_choice == 'SCISSORS' and computer_choice == 'PAPER') or
+        (user_choice == 'PAPER' and computer_choice == 'ROCK')):
         print('You win!')
     else:
-        print('Computer wins!, YOU LOSE!')
-    
-    should_continue = input('Do you want to play again? (y/n): ').lower()
-    if should_continue != 'y':
-        print('Thanks for playing!')
-        break
+        print('Tou lose')
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input('Continue? (y/n): ').lower()
+
+play_game()
--- qr_code_maker.py
+++ qr_code_maker.py
@@ -13,6 +13,6 @@ qr.add_data(url)
 img = qr.make_image()
 img.save(file_path)
 
-print(f"QR code generated and saved to {file_path}")
+print(f"QR code {qr_name} is saved to {file_path}")
 
 #add a interface and be possible to scan other qr code and convert them back to url and show the url in the interface
